7a015e5a4991d18502879abfaa48fae9ff2c907e
Kutlu Sahin <ksahin@sdl.com>
2018-06-29 16:59:00 +0200

    gfd

---
+++ transition-list/src/transition.js
@@ -0,0 +1,34 @@
+import { getDiffieHellman } from "crypto";
+
+const defaultOptions = {
+
+};
+
+function getDiff({ oldList, newList }) {
+
+}
+
+function pipe(...arrFn) {
+	return function(initialValue) {
+		arrFn.reduce(function(acc, fn) {
+			return fn(acc);
+		}, initialValue);
+	}
+}
+
+function get
+
+
+function transition(element, options = {}) {
+	let oldList = options.list || [];
+	function setList(newList) {
+
+	}
+
+
+	return {
+		setList
+	}
+}
+
+export default transition;
